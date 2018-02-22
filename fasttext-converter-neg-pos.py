b11cec3abfe6ba80290f9128850bfdb0dd5ce599
Alexandru Mahmoud <almahmoud@vassar.edu>
2018-02-21 23:16:34 -0500

    New REST-like version

--- fasttext-converter-neg-pos.py
+++ fasttext-converter-neg-pos.py
@@ -1,9 +1,8 @@
-import tempfile
+import datetime
 import json
 from flask import Flask
-import sys
+from flask import request
 
-args = sys.argv
 app = Flask(__name__)
 
 def filter_blanks(dataset):
@@ -16,38 +15,117 @@ def getfield(dataset, field):
     else:
         return filter_blanks([row[field] for row in dataset])
 
+## Print the comma-seperated log portion as a table
+def input_log_table(info):
+    header = "{:10}{:20}{}".format("InputID","Date","FilePath")
+    table = [header]
+    for (inID,date,path) in info[1:].split(','):
+        table.append("{:10}{:20}{}".format(inID,date,path))
+    return '\n'.join(table)
 
-@app.route('/convert/pos-neg/')
+
+@app.route('/convert/pos-neg', methods=['POST','GET'])
 def execute():
-    if "-p" not in args:
-        return "ERROR: No positive examples file was given"
+    pos = request.form['pos']
+    if pos is None:
+        return "ERROR: No positive examples file was given\n"
+    
+    json_pos = json.loads(pos)
 
-    input_pos = args[args.index("-p")+1]
-    with open(input_pos, 'r') as f:
-        json_pos = json.load(f)
+    neg = request.form['neg']
+    if neg is None:
+        return "ERROR: No negative examples file was given\n"
 
-    if "-n" not in args:
-        return "ERROR: No negative examples file was given"
+    json_neg = json.loads(neg)
 
-    input_neg = args[args.index("-n")+1]
-    with open(input_neg, 'r') as f:
-        json_neg = json.load(f)
+    lab_text = request.args.get("lab", default=None)
+    if lab_text is None:
+        return "ERROR: A valid list of labels must be given, in order to create appropriate temporary files for fasttext\n"
 
-    if "-l" not in args:
-        return "ERROR: A valid list of labels must be given, in order to create appropriate temporary files for fasttext"
-
-    labels = args[args.index("-l")+1].split(',')
+    lab = lab_text.split(",")
 
     all_labels = list()
 
-    for each in labels:
+    for each in lab:
         pos_data = getfield(json_pos, each)
         pos_labels = [('__label__pos %s' % ex) for ex in pos_data]
         neg_data = getfield(json_neg, each)
         neg_labels = [('__label__neg %s' % ex) for ex in neg_data]
         all_labels += pos_labels + neg_labels
 
-    return '\n'.join(all_labels)
+
+    current_id = 1
+    log_info = []
+
+    input_id = request.args.get("id", default=None)
+
+    name = request.args.get("name", default=None)
+    
+    if input_id == None:
+        try:
+            with open('data/cache/inputs/log.txt', 'r') as log:
+                log_info = log.readlines()
+                current_id = int(log_info[0].strip()) + 1
+                log_info[0] = str(current_id) + "\n"
+
+        except IOError:
+            log_info.append("1\n")
+
+        if name is None:
+            name = 'input' + current_id + '.ftxt'
+        current_file = 'data/cache/inputs/' + name
+
+        with open(current_file, 'w') as f:
+            f.write('\n'.join(all_labels))
+
+        log_info.append(str(current_id) + "," + datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + "," + current_file + '\n')
+
+        with open('data/cache/inputs/log.txt', 'w') as log:
+            f.write(''.join(log_info))
+
+    else:
+        current_id = int(input_id)
+
+        try:
+            with open('data/cache/inputs/log.txt', 'r') as log:
+                log_info = log.readlines()
+                if current_id < len(log_info):
+                    line = log_info[current_id].split(",")
+                    if name is None:
+                        name = line[2]
+                    current_file = 'data/cache/inputs/' + name
+                    line[2] = current_file
+                    line[1] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+                    log_info[current_id] = ",".join(line)
+
+                ## If the given ID is higher than the current highest, use the next available ID
+                else:
+                    current_id = len(log_info)
+                    log_info[0] = str(current_id) + "\n"
+                    if name is None:
+                        name = 'input' + current_id + '.ftxt'
+                    current_file = 'data/cache/inputs/' + name
+                    log_info.append(str(current_id) + "," + datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + "," + current_file + '\n')
+
+
+        except IOError:
+            current_id = 1
+            log_info.append("1\n")
+            if name is None:
+                current_file = 'data/cache/inputs/input1.fstx'
+            else:
+                current_file = 'data/cache/inputs/' + name
+            log_info.append("1," + datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + "," + current_file + '\n')
+
+        with open(current_file, 'w') as f:
+            f.write('\n'.join(all_labels))
+
+        with open('data/cache/inputs/log.txt', 'w') as log:
+            f.write(''.join(log_info))  
+
+    instr = "Use InputID {} for the trainer tool.\n\n".format(current_id)
+    
+    return instr + input_log_table(log_info[1:])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- fasttext-tester.py
+++ fasttext-tester.py
@@ -1,23 +1,52 @@
 import fasttext
 import tempfile
 from flask import Flask
-import sys
+from flask import request
 
-args = sys.argv
+
 app = Flask(__name__)
 
 @app.route('/tester/')
 def execute():
-    if "-t" not in args:
-        return "ERROR: No input test file was given"
+    test_id = request.args.get("test", default=None)
+    if test_id is None:
+        return "ERROR: No InputID was given for the test file."
 
-    test_file = args[args.index("-t")+1]
+    else:
+        try:
+            with open('data/cache/inputs/log.txt', 'r') as log:
+                log_info = log.readlines()
+                ## If the given ID is higher than the current highest, use the next available ID
+                if test_id < len(log_info):
+                    test_file = log_info[test_id].split(",")[2]
 
-    if "-m" not in args:
-        return "ERROR: No model was given"
+                else:
+                    instr = "ERROR: The given InputID ({}) does not correspond to any input file. The table below shows the current input files.\n\n".format(current_id)
+                    return instr + log_as_table(log_info[1:])
 
-    model_file = args[args.index("-m")+1]
-    
+        except IOError:
+            return "There is no log file for inputs. You can use the converter to create fasttext input files."
+
+
+    model_id = request.args.get("model", default=None)
+    if model_id is None:
+        return "ERROR: No ModelID was given for the model file."
+
+    else:
+        try:
+            with open('data/cache/models/log.txt', 'r') as log:
+                log_info = log.readlines()
+                ## If the given ID is higher than the current highest, use the next available ID
+                if model_id < len(log_info):
+                    model_file = log_info[model_id].split(",")[3]
+
+                else:
+                    instr = "ERROR: The given ModelID ({}) does not correspond to any model. The table below shows the existing models.\n\n".format(current_id)
+                    return instr + log_as_table(log_info[1:])
+
+        except IOError:
+            return "There is no log file for models. You can use the trainer to create fasttext models."
+
     classifier = fasttext.load_model(model_file)
     result = classifier.test(test_file)
     return str(vars(result))
